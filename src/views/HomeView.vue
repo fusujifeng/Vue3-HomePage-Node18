043308e5daec95f016180f601c294a861f32d0e3
fusujifeng <71201851+fusujifeng@users.noreply.github.com>
2023-07-29 15:23:10 +0800

    feat: 初始化vue3 vite tailwindcss daisyui 环境

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-amber-200 p-8 rounded">
+    <p>22444</p>
+  </div>
+  <label class="swap swap-flip text-9xl">
+
+    <!-- this hidden checkbox controls the state -->
+    <input type="checkbox" />
+
+    <div class="swap-on">😈</div>
+    <div class="swap-off">😇</div>
+  </label>
+</template>
