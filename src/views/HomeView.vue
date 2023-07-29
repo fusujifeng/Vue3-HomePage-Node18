01f391cee1ba756c20992656eeb2d2de29a3e61e
fusujifeng <71201851+fusujifeng@users.noreply.github.com>
2023-07-29 15:43:46 +0800

    feat: 增加Footer，去除vue3自带样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,8 @@
 <script setup>
+import Footer from "@/views/Footer/Footer.vue";
 </script>
 
 <template>
-  <div class="bg-amber-200 p-8 rounded">
-    <p>22444</p>
-  </div>
-  <label class="swap swap-flip text-9xl">
 
-    <!-- this hidden checkbox controls the state -->
-    <input type="checkbox" />
-
-    <div class="swap-on">😈</div>
-    <div class="swap-off">😇</div>
-  </label>
+  <Footer/>
 </template>
