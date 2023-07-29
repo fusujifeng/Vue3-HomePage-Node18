0467555eaf6bf44fdd8bad94da4d29c9b3f4877e
fusujifeng <71201851+fusujifeng@users.noreply.github.com>
2023-07-29 16:41:36 +0800

    增加客户案例组件

---
+++ src/views/CustomerCase/CustomerCase.vue
@@ -0,0 +1,95 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="text-gray-600 body-font">
+    <div class="container px-12 py-14 mx-auto " >
+      <div class="flex flex-col text-center w-full mb-20">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">客户案例</h1>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+      </div>
+      <div class="flex flex-wrap -m-4">
+        <div class="lg:w-1/4 sm:w-1/2 p-4 ">
+          <div class="flex relative roundedShadow">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/600x360/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative roundedShadow">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/601x361/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/603x363/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/602x362/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/605x365/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/606x366/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/606x366/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/4 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center roundedShadow" src="https://source.unsplash.com/606x366/?flower">
+            <div class="px-8 py-10 relative z-10 w-full  bg-white opacity-0 hover:opacity-100 roundedShadow">
+              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.roundedShadow{
+  @apply rounded-xl drop-shadow-2xl
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup>
+import Navbar from "@/views/Navbar/Navbar.vue";
+import Hero from "@/views/Hero/Hero.vue";
+import CustomerCase from "@/views/CustomerCase/CustomerCase.vue";
 import Footer from "@/views/Footer/Footer.vue";
+
+
 </script>
 
 <template>
+  <!--  https://source.unsplash.com/720x600/?flower-->
 
+  <Navbar/>
+  <Hero/>
+  <CustomerCase/>
   <Footer/>
 </template>
