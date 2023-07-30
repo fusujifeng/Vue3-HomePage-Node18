a707a0286fc1e10ab6f984798ecbca97e6e798aa
fusujifeng <71201851+fusujifeng@users.noreply.github.com>
2023-07-30 15:09:18 +0800

    feat:Navbar菜单更新

--- src/views/CustomerCase/CustomerCase.vue
+++ src/views/CustomerCase/CustomerCase.vue
@@ -7,7 +7,7 @@
     <div class="container px-12 py-14 mx-auto " >
 
       <div class="flex flex-col text-center w-full mb-20">
-        <button class="btn btn-primary mx-auto m-4">客户案例</button>
+        <button class="btn btn-primary mx-auto m-4 text-2xl">客户案例</button>
         <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
       </div>
       <div class="flex flex-wrap -m-4">
