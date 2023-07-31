1528f842e9f6d33154583c4b5fa54c8dec4f42a5
chenhang <1362320337@qq.com>
2023-07-31 09:20:00 +0800

    开放vite局域网访问

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    //使用IP能访问
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
